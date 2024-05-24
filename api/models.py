847c461769f38e5978769e2dacdd2807abd8853a
Anish Gharti <anishDGharti@gmail.com>
2024-05-24 10:02:17 +0545

    testings

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
-from django.db import models
 
-# Create your models here.
+
+
+
+# from django.db import models
+# from django.contrib.auth.models import AbstractUser
+
+# class User(AbstractUser):
+#     profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+
+#     def __str__(self):
+#         return self.username if self.username else self.email 
